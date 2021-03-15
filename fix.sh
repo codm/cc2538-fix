a38f1d2644a9b77fef0e70fbb620ac3b90212705
Tobias Schmitt <tschmittldk@gmail.com>
2021-03-15 09:00:25 +0000

    added config print and zigbee2mqtt handling

--- fix.sh
+++ fix.sh
@@ -6,25 +6,48 @@ source fix.conf
 
 checkout_and_start_zigbee2mqtt()
 {
-    git clone $FIX_GITHUB .
-    npm install
-    if [ $? != 0 ];
-    then
-        echo "Error in NPM install, bye"
-        exit 2
-    fi
+# TODO: uncomment this when releasing
+#    git clone $FIX_GITHUB .
+#    npm install
+#    if [ $? != 0 ];
+#    then
+#        echo "Error in NPM install, bye"
+#        exit 2
+#    fi
+    echo "Setting config"
+    cat <<EOF > data/configuration.yaml
+homeassistant: false
+
+permit_join: false
+
+mqtt:
+    base_topic: zigbee2mqtt
+    server: 'mqtt://localhost'
+serial:
+    port: $FIX_UART_DEVICE
+EOF
+    echo "starting zigbee2mqtt for 60 seconds, logging to zigbee2mqtt_output.log"
+    npm start > $STARTING_DIR/zigbee2mqtt_output.log 2>&1 &
+    ZIG2MQTTPID=$!
+    echo "wait for 60 seconds for zigbee2mqtt, then kill $ZIG2MQTTPID"
+    sleep 60
+    kill -15 $ZIG2MQTTPID
     # Run zigbee-herdsman to create a coordinator-backup.json
 }
 
+
 # Script Entry point
+
+echo $!
 STARTING_DIR=`pwd`
 echo "1: Create Tempdir"
-if [ -d $FIX_TEMPDIR ];
-then
-    echo "Target dir $FIX_TEMPDIR exists, please delete it first or choose another directory"
-    exit 0
-fi
-mkdir -p $FIX_TEMPDIR
+# TODO: uncomment this when releasing
+#if [ -d $FIX_TEMPDIR ];
+#then
+#    echo "Target dir $FIX_TEMPDIR exists, please delete it first or choose another directory"
+#    exit 0
+#fi
+#mkdir -p $FIX_TEMPDIR
 if [ ! -d $FIX_TEMPDIR ];
 then
     echo "Something went wrong creating the tempdir"
